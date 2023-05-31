be45eb4bd520957e71bd7bc011e568aebd23def8
Denssed <rodriguezp.jeremys@gmail.com>
2023-05-31 15:24:34 -0500

    creation components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './Components/login/login.component';
+import { UserComponent } from './Components/user/user.component';
+import { AppoimentComponent } from './Components/appoiment/appoiment.component';
+import { TicketComponent } from './Components/ticket/ticket.component';
+import { ScheduleComponent } from './Components/schedule/schedule.component';
+import { MedsComponent } from './Components/meds/meds.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //General
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+  //User
+  {path: 'user', component: UserComponent},
+  {path: 'appoiment', component: AppoimentComponent},
+  {path: 'ticket', component: TicketComponent},
+  //Meds
+  {path: 'schedule', component: ScheduleComponent},
+  //Admin
+  {path: 'doctors', component: MedsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './Components/navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './Components/login/login.component';
+import { UserComponent } from './Components/user/user.component';
+import { AppoimentComponent } from './Components/appoiment/appoiment.component';
+import { TicketComponent } from './Components/ticket/ticket.component';
+import { ScheduleComponent } from './Components/schedule/schedule.component';
+import { MedsComponent } from './Components/meds/meds.component';
+import { NavTabComponent } from './Components/nav-tab/nav-tab.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    LoginComponent,
+    UserComponent,
+    AppoimentComponent,
+    TicketComponent,
+    ScheduleComponent,
+    MedsComponent,
+    NavTabComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
